ac1163348305d0cffc2ae095e31095b15c18991d
Olgah29@hotmail.com <olgah29@hotmail.com>
2025-03-23 17:32:01 +0300

    addition

--- jaribu.py
+++ jaribu.py
@@ -1,8 +1,6 @@
 print("hello world")
 print("Niaje")
 print("World cup 2025")
-<<<<<<< HEAD
 print("jaber nyiri")
-=======
 print("data science")
->>>>>>> 91cf8f4543c919c0353115b3e7652cf68884c6b3
+print("Kyle Baba")
